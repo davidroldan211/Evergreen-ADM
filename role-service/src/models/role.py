6a11cdf6f4ecad5d0e9b7e82f2bb24cb0c6c58d8
David Roldán <davidroldan211@gmail.com>
2023-09-22 12:36:47 -0500

    Delete folders

--- role-service/src/models/role.py
+++
@@ -1,7 +0,0 @@
-from config.database import Base
-from sqlalchemy import Column, Integer, String, Float
-
-class Role(Base):
-    __tablename__ = 'roles'
-    id = Column(Integer, primary_key=True)
-    role = Column(String)
